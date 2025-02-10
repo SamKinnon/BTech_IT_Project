863906869971a62abc2a8fbf8b7fb9058c631c14
SamKinnon <garysamjones@gmail.com>
2025-02-10 18:15:22 +0200

    download current file

--- index.js
+++ index.js
@@ -32,3 +32,13 @@ scrollLinks.forEach(link => {
         }
     });
 });
+document.querySelector("#toggle-theme").addEventListener("click", () => {
+    document.documentElement.classList.toggle("light-theme");
+  });
+  
+  const prefersLight = window.matchMedia(
+    "(prefers-color-scheme: light)"
+  )?.matches;
+  if (prefersLight) {
+    document.documentElement.classList.toggle("light-theme");
+  }
